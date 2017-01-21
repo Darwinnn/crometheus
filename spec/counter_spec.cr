3a67393cffc0fd06c4264f1a88110998e5addaec
Ezra Stevens <ezrastevens@cloudandtree.com>
2017-01-20 16:55:04 -0800

    add Counters

--- spec/counter_spec.cr
+++ spec/counter_spec.cr
@@ -27,4 +27,35 @@ describe Crometheus::Collector(Crometheus::Counter) do
     end
   end
 
+  describe "#count_exceptions" do
+    it "increments when the block raises an exception" do
+      counter.reset
+      10.times do |ii|
+        begin
+          counter.count_exceptions {raise ArgumentError.new if ii % 2 == 0}
+        rescue ex : ArgumentError
+        end
+      end
+      counter.get.should eq 5.0
+    end
+
+    it "re-raises the exception" do
+      expect_raises(ArgumentError) {counter.count_exceptions {raise ArgumentError.new}}
+    end
+  end
+
+  describe ".count_exception_type" do
+    it "increment when the block raises the given type of exception" do
+      exceptions = [ArgumentError.new, KeyError.new, DivisionByZero.new,
+        ArgumentError.new, ArgumentError.new]
+      counter.reset
+      exceptions.each do |ex|
+        expect_raises do
+          Crometheus::Counter.count_exception_type(counter, ArgumentError) {raise ex}
+        end
+      end
+      counter.get.should eq 3.0
+    end
+  end
+
 end
--- src/crometheus/counter.cr
+++ src/crometheus/counter.cr
@@ -1,19 +1,51 @@
 require "./metric"
 
 module Crometheus
+  # A Metric type that stores a value internally. This value increases
+  # monotonically, except that it can be reset to zero.
   class Counter < Metric
     @value = 0.0
 
+    # Fetches @value.
     def get
       @value
     end
 
+    # Increments @value by the given amount.
     def inc(x : Int | Float = 1.0)
       @value += x
     end
 
+    # Sets @value to 0.0.
     def reset
       @value = 0.0
     end
+
+    # Increments @value when the block raises an exception.
+    def count_exceptions(ex_type = Exception)
+      yield
+    rescue ex
+      inc
+      raise ex
+    end
+
+    def type
+      "counter"
+    end
+
+    # Increments the given counter when the block raises an exception
+    # matching the given type. Once
+    # https://github.com/crystal-lang/crystal/issues/2060 is resolved,
+    # this macro will be deprecated and its functionality folded into
+    # #count_exceptions.
+    macro count_exception_type(counter, ex_type)
+      begin
+        {{yield}}
+      rescue ex : {{ex_type}}
+        counter.inc
+        raise ex
+      end
+    end
+
   end
 end
