860f5140f33014488692421be3aac7e3ac8dd249
Ezra Stevens <ezrastevens@cloudandtree.com>
2017-02-19 00:55:55 -0800

    update example

--- examples/src/wordcounter.cr
+++ examples/src/wordcounter.cr
@@ -8,8 +8,6 @@ include Crometheus
 
 # Initialize some new unlabelled metrics, passing a name and docstring to each
 lines = Counter.new(:lines, "The number of lines entered")
-start_time = Gauge.new(:start_time, "Startup timestamp")
-start_time.set_to_current_time
 last_usage = Gauge.new(:last_usage, "Timestamp of latest gets()")
 last_usage.set_to_current_time
 
